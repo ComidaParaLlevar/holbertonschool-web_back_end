b03dc2f582b15381218a30a4cf27ca3a17dbc8c4
Lyan <lyanosorio98@gmail.com>
2024-04-22 09:53:57 -0400

    added 0-add.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function that adds two integers, returns sum as float"""
+
+
+def add(a: float, b: float) -> float:
+    """returns sum of two floats as float"""
+    return a + b
