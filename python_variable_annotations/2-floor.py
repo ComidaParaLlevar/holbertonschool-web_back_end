06352d2b20213f95754ea17ca4e389075b1fbabb
Lyan <lyanosorio98@gmail.com>
2024-04-23 17:55:47 -0400

    added 3-to_str.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""type-annotated function takes float and returns floor of float"""
+import math
+
+
+def floor(n: float) -> int:
+    """returns floor of float"""
+    return math.floor(n)
