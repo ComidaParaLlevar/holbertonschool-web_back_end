c3ef57b5c54bdd894e1b7782654baad1db6bbac9
Lyan <lyanosorio98@gmail.com>
2024-04-24 14:55:39 -0400

    added 0-basic_async_syntax

---
+++ python_variable_annotations/python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""type-annotated coroutine that takes in an integer argument"""
+import asyncio
+import random
+
+def wait_random(max_delay: int) -> float:
+    """returns random float number"""
+    return random.uniform(0, max_delay)
