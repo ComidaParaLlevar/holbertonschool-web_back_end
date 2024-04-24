9a255d30cfa226234e1562e4e14b115f9dd24701
Lyan <lyanosorio98@gmail.com>
2024-04-24 11:13:47 -0400

    added 2 lines after import

--- python_variable_annotations/9-element_lenght.py
+++ python_variable_annotations/9-element_lenght.py
@@ -2,6 +2,7 @@
 """return values with correct types"""
 from typing import List, Tuple, Iterable, Sequence
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """returns list of tuples with string and int"""
     return [(i, len(i)) for i in lst]
