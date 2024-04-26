99220b83a754ecdc336cc8b2b308d775b650548f
Lyan <lyanosorio98@gmail.com>
2024-04-25 20:30:03 -0400

    imported whole time library instead of function

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,14 +3,14 @@
 import asyncio
 import random
 from typing import List
-from time import perf_counter
+import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
     """function that measures the runtime of wait_n"""
-    start = perf_counter()
+    start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    end = perf_counter()
+    end = time.perf_counter()
     return (end - start) / n
