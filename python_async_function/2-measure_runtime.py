d9c4ca52085c86189ccacd8499661bec7ab5ff78
Lyan <lyanosorio98@gmail.com>
2024-04-25 20:11:59 -0400

    added 2-measure_runtime.py

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Function that measures the runtime of wait_n"""
+import asyncio
+import random
+from typing import List
+from time import perf_counter
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """function that measures the runtime of wait_n"""
+    start = perf_counter()
+    await wait_n(n, max_delay)
+    end = perf_counter()
+    return (end - start) / n
