176978f972f6a3172078d415ecfe5ed495bdf452
Lyan <lyanosorio98@gmail.com>
2024-04-25 20:34:00 -0400

    deleted async keyword

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 """Function that measures the runtime of wait_n"""
 import asyncio
-import random
-from typing import List
 import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """function that measures the runtime of wait_n"""
     start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
