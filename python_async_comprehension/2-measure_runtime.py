b2702fc0a86c58d6c85ec2c3865c1c56b78fe354
Lyan <lyanosorio98@gmail.com>
2024-04-26 11:34:11 -0400

    changed file name

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    ''' Function to coutne '''
+    ''' Function to measure runtime of various comprehensions '''
     start = time.perf_counter()
     await asyncio.gather(async_comprehension(), async_comprehension(),
                          async_comprehension(), async_comprehension())
